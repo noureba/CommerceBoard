2f4521906a270b220681fa2c769d1d918ea4b39a
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-14 17:13:05 +0100

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/global.css";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">hello work</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./public",
+    port: 3000,
+  },
+};
